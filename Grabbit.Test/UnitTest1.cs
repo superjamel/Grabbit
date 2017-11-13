657ec4106b5809cc92f8357162ae6e99afcb6324
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-13 10:18:01 +0100

    Add project start

---
+++ Grabbit.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grabbit.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
